75f4645ed2e5a735072a33bd89a3a75d75fee19d
Yustiniya Khokhlova <yustiniya.i.khokhlova@gmail.com>
2023-08-29 15:01:15 +0200

    fixed seeds

--- app/controllers/rollercoasters_controller.rb
+++ app/controllers/rollercoasters_controller.rb
@@ -3,4 +3,8 @@ class RollercoastersController < ApplicationController
     @rollercoaster = Rollercoaster.new
     @rollercoaster.save
   end
+
+  def index
+    @rollercoasters = Rollercoaster.all
+  end
 end
