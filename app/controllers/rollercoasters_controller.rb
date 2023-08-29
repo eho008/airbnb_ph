e2ac5cf32df8e9f796498aaacc01334e0f66a705
Yustiniya Khokhlova <yustiniya.i.khokhlova@gmail.com>
2023-08-29 16:14:46 +0200

    seeded photos, arrange grid on homepage

--- app/controllers/rollercoasters_controller.rb
+++ app/controllers/rollercoasters_controller.rb
@@ -20,10 +20,7 @@ class RollercoastersController < ApplicationController
   private
 
   def rollercoaster_params
-    params.require(:rollercoaster).permit(:name, :location, :price, :description)
+    params.require(:rollercoaster).permit(:name, :location, :price, :description, photos: [])
   end
 
-  def index
-    @rollercoasters = Rollercoaster.all
-  end
 end
--- app/models/rollercoaster.rb
+++ app/models/rollercoaster.rb
@@ -1,5 +1,7 @@
 class Rollercoaster < ApplicationRecord
   belongs_to :user
   has_many :bookings
+  has_many_attached :photos
   validates :name, :location, :price, :description, presence: true
+
 end
