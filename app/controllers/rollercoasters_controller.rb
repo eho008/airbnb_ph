53cdb26fb3c78dd092703f96c582e06908588a7e
SaraMartins <saraluis88@gmail.com>
2023-08-29 13:04:25 +0200

    rollercoasters controllers and views

--- app/controllers/rollercoasters_controller.rb
+++ app/controllers/rollercoasters_controller.rb
@@ -1,2 +1,25 @@
 class RollercoastersController < ApplicationController
+
+  def index
+    @rollercoasters = Rollercoaster.all
+  end
+
+  def new
+    @rollercoaster = Rollercoaster.new
+  end
+
+  def create
+    @rollercoaster = Rollercoaster.new(rollercoaster_params)
+    if @rollercoaster.save
+      redirect_to @rollercoaster, notice: 'Your rollercoaster was created!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def rollercoaster_params
+    params.require(:rollercoaster).permit(:name, :location, :price, :description)
+  end
 end
