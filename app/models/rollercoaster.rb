71da5de649ed563bb372b4ad22b629bfad1e14c0
Jode Koporo <jkoporo@gmail.com>
2023-08-31 11:25:59 +0100

    search

--- app/models/rollercoaster.rb
+++ app/models/rollercoaster.rb
@@ -5,4 +5,10 @@ class Rollercoaster < ApplicationRecord
   has_many_attached :photos
   validates :name, :location, :price, :description, presence: true
 
+  include PgSearch::Model
+  pg_search_scope :search_by_info,
+  against: [ :name, :location, :price, :description ],
+  using: {
+    tsearch: { prefix: true }
+  }
 end
