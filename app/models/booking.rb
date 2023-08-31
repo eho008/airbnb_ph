3e0513479868b66b15610ef4b8083c903a17ef9d
Duarte Martins <duartempmartins@gmail.com>
2023-08-31 11:50:30 +0100

    edit, delete, validations and refactor

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,7 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :rollercoaster
   belongs_to :user
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
   validates :date_begin, presence: true
   validates :date_end, comparison: { greater_than: :date_begin }, presence: true
 end
--- app/models/rollercoaster.rb
+++ app/models/rollercoaster.rb
@@ -1,8 +1,7 @@
 class Rollercoaster < ApplicationRecord
   belongs_to :user
-  has_many :bookings
-  has_many :reviews, through: :bookings
+  has_many :bookings, dependent: :destroy
+  has_many :reviews, through: :bookings, dependent: :destroy
   has_many_attached :photos
   validates :name, :location, :price, :description, presence: true
-
 end
