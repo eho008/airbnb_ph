304254ed8ca87a7119236c580488476b229dd80c
Elias Harder <elias941601@gmail.com>
2023-08-31 17:08:51 +0200

    avatar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,9 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
-
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:photo])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:photo])
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,10 @@ class UsersController < ApplicationController
     @bookings = Booking.all
     @rollercoasters = Rollercoaster.all
   end
+
+  private
+
+  def article_params
+    params.require(:users).permit(:avatar)
+  end
 end
